4c41c3006e8d696625ab61f5f8af81760cc4ecc5
mypinkychom <mypinky.chom@gmail.com>
2025-06-14 22:59:13 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,20 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('v1').then(cache => {
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('exo-l-cache').then(function(cache) {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
+        'index.html',
+        '66143.jpg',
+        'manifest.json',
+        'icon.png'
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
